1e0686320b44fa4833d1e7671beb6a10b14f4a47
Emmanuel Nnajiofor Chinonye <emiklad1993@gmail.com>
2020-07-06 14:27:12 +0100

    Updates

---
+++ precache-manifest.f32cebf771be564557781f8bd21d8215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0105490ee472ed2de71d75ad618b9697",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e6401ba3e3defb5f09df",
+    "url": "/monsters-rolodex/static/css/main.49e8053b.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6401ba3e3defb5f09df",
+    "url": "/monsters-rolodex/static/js/main.39db94ca.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
